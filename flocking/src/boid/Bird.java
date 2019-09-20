52ab12b819ccb5a73351966cab9b9ae7c4f82606
AlexanderLuasan <38295434+AlexanderLuasan@users.noreply.github.com>
2019-09-20 22:27:17 +0800

    doing a bit of setup for boids made a test set for vector creation

--- flocking/src/boid/Bird.java
+++ flocking/src/boid/Bird.java
@@ -1,5 +1,23 @@
 package boid;
 
+import vector.Vector;
+
 public class Bird implements Boid {
+
+
+	public boolean movement() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void seeBoid(Boid other) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Vector getVelocityVector() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 }
--- flocking/src/boid/Boid.java
+++ flocking/src/boid/Boid.java
@@ -1,5 +1,10 @@
 package boid;
 
+import vector.Vector;
+
 public interface Boid {
-	//public abstract boolean movement();
+	public abstract boolean movement();
+	public abstract void seeBoid(Boid other);
+	public abstract Vector getVelocityVector();
+	
 }
