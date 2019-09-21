0cb113f1d934015b42604b1268449beb93cf8353
AlexanderLuasan <38295434+AlexanderLuasan@users.noreply.github.com>
2019-09-21 22:00:39 +0800

    first work on boid and bird

--- flocking/src/boid/Bird.java
+++ flocking/src/boid/Bird.java
@@ -2,34 +2,147 @@ package boid;
 
 import vector.Vector;
 
+/*
+ * class Bird{
+ * 		Vector position the boids position
+ * 		Vector velocity the boids velocity
+ * 		Vector acceleration the acceleration of the boid
+ * 
+ * 		Bird(int x,int y,vector vel);	//position x and y and inital velocity
+ * 		bool movement();				//update the birds position to the next frame
+ * 		seeboid(boid other);			//how to tell a boid about other boids
+ * 										//this triggers all differnt rules
+ * 		behavior();						//finish all the calulations
+ * 										//this finishes the calulaition for all the rules
+ * 		other general functions are discriped in boid interface		
+ * 
+ * 		//rule functions
+ * 			//alignment
+ * 				Vector alignment;
+ * 				int alignmentCount
+ * 				void align(boid other);		//add a boids velocity vector the alignment vector
+ * 				void align();				//finish the calulation and add it to the acceleration
+ * 			//Cohesion
+ * 				Vector cohesion;
+ * 				int cohesionCount
+ * 				void cohesion(boid other);		//add a boids velocity vector the cohesion vector
+ * 				void cohesion();				//finish the calulation and add it to the acceleration
+ *			//seperation
+ *				Vector seperation;
+ * 				int seperationCount
+ * 				void cohesion(boid other);		//add a boids velocity vector the seperation vector
+ * 				void cohesion();				//finish the calulation and add it to the acceleration
+ *			
+ * 		
+ * }
+ */
+
 public class Bird implements Boid {
+	private Vector position;
+	private Vector velocity;
+	private Vector acceleration;
+	
+	//rule vectors
+	private Vector alignment=new Vector(0,0);
+	private int alignmentCount=0;
+	private Vector separation=new Vector(0,0);
+	private int separationCount=0;
+	private Vector cohesion=new Vector(0,0);
+	private int cohesionCount=0;
 
-
+	public Bird(int x, int y, Vector vel) {
+		position = new Vector((double)x,(double)y);
+		velocity = vel;
+		acceleration = new Vector(0,0);
+	}
+	
 	public boolean movement() {
-		// TODO Auto-generated method stub
-		return false;
+		//needed limit max acceleration
+		//update the velocity with the acceleration
+		velocity = velocity.add(acceleration);
+		
+		//update the position with the velocity
+		position = position.add(velocity);
+		
+		
+		//needed limit max speed
+		//needed rollover world edge
+		
+		return true;
 	}
 
 	public void seeBoid(Boid other) {
-		// TODO Auto-generated method stub
+		
+		this.align((Bird)other);
+		this.cohesion((Bird)other);
+		this.separation((Bird)other);
 		
 	}
+	
 
+	public boolean behaviour() {
+			
+		this.align();
+		this.cohesion();
+		this.separation();
+		return false;
+	}
+	
+	//alignment
+	void align(Bird other) {
+		alignment.add(other.velocity);
+		alignmentCount+=1;
+	}
+	void align() {
+		//unimplemented
+	}
+	
+	//cohesion
+	void cohesion(Bird other) {
+		this.cohesion.add(other.position);
+		this.cohesionCount+=1;
+	}
+	void cohesion() {
+		
+	}
+	
+	void separation(Bird other) {
+		//this.seperation.add(other.position);
+		//this.seperationCount+=1;
+	}
+	void separation() {
+		
+	}
+	
+	
+	@Override
 	public Vector getVelocityVector() {
-		// TODO Auto-generated method stub
-		return null;
+		return velocity;
 	}
 
 	@Override
 	public Vector getPositionVector() {
-		// TODO Auto-generated method stub
-		return null;
+		return position;
 	}
 
-	@Override
-	public boolean behaviour() {
-		// TODO Auto-generated method stub
-		return false;
+
+
+	protected Vector getPosition() {
+		return position;
 	}
+
+	protected void setPosition(Vector position) {
+		this.position = position;
+	}
+
+	protected Vector getVelocity() {
+		return velocity;
+	}
+
+	protected void setVelocity(Vector velocity) {
+		this.velocity = velocity;
+	}
+	
+	//alignment
 	
 }
