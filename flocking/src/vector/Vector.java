1d5dbecb0337df665a0e08dc7c18d3d346949834
jonathanKuo <jonathan.kuo33@gmail.com>
2019-09-21 16:51:48 +0800

    some minor changes on Vector

--- flocking/src/vector/Vector.java
+++ flocking/src/vector/Vector.java
@@ -11,11 +11,16 @@ package vector;
  * 		public Vector(double x,double y);
  * 		public Vector(double radians, double length);
  * 		public Vector(double x1, double y1,double x2, double y2); //use the other constructor
- * 		public rotate(double radians); //increase the angle could be negative -- if this function seem to slow you might need to also hold the polar cordinates to save time
+ * 		public rotate(double radians); //increase the angle could be negative -- if this function seem to slow you might need to also hold the polar coordinates to save time
  * 										angle + 1 is much faster than tan cos sin stuff i think
  * 		public Vector limit(double min, double max); // adjust length so it is within the range
  * }
  */
+
+
 public class Vector {
 	
+	public Vector add(Vector other) {
+		return other;
+	}
 }
