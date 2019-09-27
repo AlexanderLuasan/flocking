c931ef0ffbf52fcc94091167104ba765f1426e72
jonathanKuo <jonathan.kuo33@gmail.com>
2019-09-27 17:32:27 +0800

    i think trace is done

--- flocking/src/ray/Ray.java
+++ flocking/src/ray/Ray.java
@@ -1,5 +1,9 @@
 package ray;
+import java.util.ArrayList;
+import java.util.Collections;
+
 import vector.Vector;
+import shape.Circle;
 /* 
 class ray{
 
@@ -8,13 +12,15 @@ class ray{
 	ok	Vector direction;
 	ok	double distanceLast;
 
-	un	double trace(distance x);				//run the trace for a minimum unit return distance if you hit anything, -1 for hitting nothing 
-	un	double fetchDistance();					//what was the actual distance
-	un	arrayList<shape.Circles> fetchCirlces(); 	//calculate all the circles for drawing and debuging reuse the shape class for esay drawing
-	un	bool search(double distance);		//rotate the direction vector left and right until you find a direction that dose not colide within the distance save the vector to direction
+	un	double trace(double limitDistance);			//run the trace for a minimum unit return distance if you hit anything, -1 for hitting nothing 
+	un	double fetchDistance();						//what was the actual distance
+	un	arrayList<shape.Circles> fetchCirlces(); 	//calculate all the circles for drawing and debugging reuse the shape class for easy drawing
+	un	boolean search(double distance);			//rotate the direction vector left and right until you find a direction that does not collide within the distance save the vector to direction
 }
  */
 public class Ray {
+	
+	public static final ArrayList<RayDetectable> rayDetectable = new ArrayList<RayDetectable>(); 
 	
 	private Vector startPoint;
 
@@ -56,6 +62,35 @@ public class Ray {
 		this.lastDistance = lastDistance;
 	}
 
+	public double trace(double limitDistance) {
+		double tempDistance = 0;
+		double newDistance = 0;
+		boolean hitObject = false;
+		while(!hitObject && newDistance<=limitDistance) {
+			tempDistance = rayDetectable.get(0).distanceToPoint(this.getCurrentPoint());
+			for(int i=0; i<rayDetectable.size()-1; i++) {
+				if(rayDetectable.get(i+1).distanceToPoint(this.getCurrentPoint()) < newDistance){
+					tempDistance = rayDetectable.get(i+1).distanceToPoint(this.getCurrentPoint());
+				}
+			}
+			newDistance+=tempDistance;
+			this.currentPoint.setLength(tempDistance);
+		}
+		return (hitObject == true) ? newDistance : -1; 
+	}
+	
+	public double fetchDistance() {
+		
+	}
+	
+	public arrayList<shape.Circles> fetchCirlces(){
+		
+	}
+	
+	public boolean search(double distance) {
+		
+	}
+	
 	@Override
 	public String toString() {
 		return "Ray [startPoint=" + startPoint + ", currentPoint=" + currentPoint + ", direction=" + direction
