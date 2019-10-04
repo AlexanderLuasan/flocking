85ac5b322b7b586b38c5a161bcecba82881cc657
AlexanderLuasan <38295434+AlexanderLuasan@users.noreply.github.com>
2019-10-04 17:03:38 +0800

    ray comments

--- flocking/src/ray/RayV2.java
+++ flocking/src/ray/RayV2.java
@@ -5,5 +5,32 @@ public class RayV2 extends Ray {
 	public RayV2() {
 		
 	}
-
+	public double trace(double limitDistance) {
+		
+		//create the perpendicular vector
+		
+		
+		//for each object
+		
+		//get its points
+		
+		//make a new copy of its points/have the shape always give a new copy
+		
+		//shift the points to put us at the center
+		
+		//check the perpendicular colision
+		
+		//project each point see if one is positive or negative
+		
+		//if so check the regular position
+		
+		//project each point see if one is positive or negative
+		
+		//if positive calulate the colost point and return
+		
+		//else move on it was behind us
+		
+		
+		return limitDistance;	
+	}
 }
