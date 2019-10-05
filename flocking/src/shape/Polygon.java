fa48726faa859c716da778d8874e39f4aef2af7c
chiatseKuo <jonathan.kuo33@gmail.com>
2019-10-05 16:43:55 +0800

    polygon getPoints

--- flocking/src/shape/Polygon.java
+++ flocking/src/shape/Polygon.java
@@ -1,10 +1,10 @@
 package shape;
-
+import ray.RayDetectable;
 import java.util.ArrayList;
 
 import vector.Vector;
 
-public class Polygon extends Shape{
+public class Polygon extends Shape implements RayDetectable{
 	ArrayList<Vector> vector_list = new ArrayList<Vector>();
 	
 	public Polygon(Vector center, ArrayList<Vector> list) {
@@ -14,6 +14,20 @@ public class Polygon extends Shape{
 	}
 	public ArrayList<Vector> getlines() {
 		return this.vector_list;
-		
+	}
+	@Override
+	public double distanceToPoint(Vector point) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public double distanceToPointCircle(Vector point) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public ArrayList<Vector> getPoints(Vector projection) {
+		// TODO Auto-generated method stub
+		return this.getlines();
 	}
 }
