5bd28ba0c244f0df3f7ea60443ec4b2d9e19645d
AlexanderLuasan <38295434+AlexanderLuasan@users.noreply.github.com>
2019-10-05 16:38:32 +0800

    edit ray detectable

--- flocking/src/ray/RayDetectable.java
+++ flocking/src/ray/RayDetectable.java
@@ -7,4 +7,5 @@ public interface RayDetectable {
 	public abstract double distanceToPoint(Vector point);//the length of the shortest path from the point to the edge of the shape
 	public abstract double distanceToPointCircle(Vector point);//the length of the shortest path from the point to the edge of the shape if it were a circle
 	public abstract ArrayList<Vector> getPoints(Vector projection);// the points of the shape
+	public abstract Vector getCenter();
 }
