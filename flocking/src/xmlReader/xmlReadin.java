5d9f84f1ee4735c9e4b0fea447d3f982bc481448
Sungho PARK <andrepark3-c@ad.cityu.edu.hk>
2019-10-07 21:39:02 +0800

    Added test tmx file and updated xmlReading

--- flocking/src/xmlReader/xmlReadin.java
+++ flocking/src/xmlReader/xmlReadin.java
@@ -1,5 +1,11 @@
 package xmlReader;
 
+import shape.*;
+import org.w3c.dom.*;
+import javax.xml.parsers.*;
+import java.io.*;
+import java.util.ArrayList;
+
 /*
 class xmlReadin{
 
@@ -25,9 +31,49 @@ class xmlReadin{
 
 
 public class xmlReadin {
-
+	private ArrayList<Shape> listofshapes = new ArrayList<Shape>();
+	private int width;
+	private int height;
+	
 	public xmlReadin(String fileLocation) {
+		try {
+			File inputFile = new File("CS3343.tmx");
+	        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+	        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+	        Document doc = dBuilder.parse(inputFile);
+	        doc.getDocumentElement().normalize();
+	        System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+	        
+	        NodeList screen = doc.getElementsByTagName("map");
+	        // should read width and height times them by 50 to get the width and height pixel
+	        
+	        NodeList nList = doc.getElementsByTagName("objectgroup");
+	        for (int temp = 0; temp < nList.getLength(); temp++) { 
+	        	Node nNode = nList.item(temp);
+	        	//add all the element in the object group to the listofshapes
+	        }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public ArrayList<Shape> getEnvironment() {
+		return this.listofshapes;
 		
 	}
-
+	
+	public ArrayList<BoidStructure> getAllboids() {
+		return null;
+		
+	}
+	
+	public int getWidth() {
+		return this.width;
+		
+	}
+	
+	public int getHeight() {
+		return this.height;
+		
+	}
 }
