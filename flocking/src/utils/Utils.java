c27cc8af80aaaaaab60c0f63d0ebe6cbb8e70f00
chiatseKuo <jonathan.kuo33@gmail.com>
2019-10-11 17:42:00 +0800

    refactor vector test

--- flocking/src/utils/Utils.java
+++ flocking/src/utils/Utils.java
@@ -3,6 +3,7 @@ package utils;
 import vector.Vector;
 
 public class Utils {
+	
 	public static int SCREEN_HIEGHT = 800;
 	public static int SCREEN_WIDTH = 1200;
 	
@@ -11,6 +12,7 @@ public class Utils {
 		input=Math.round(input);
 		return input/1000;
 	}
+	
 	public static double distance(Vector a, Vector b) {
 		double x = a.getxComponent()-b.getxComponent();
 		double y = a.getyComponent()-b.getyComponent();
