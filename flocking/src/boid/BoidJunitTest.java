106009998094e61ea81785dc32638b6653a3f788
AlexanderLuasan <38295434+AlexanderLuasan@users.noreply.github.com>
2019-10-11 16:57:01 +0800

    boid testcases

--- flocking/src/boid/BoidJunitTest.java
+++ flocking/src/boid/BoidJunitTest.java
@@ -8,27 +8,13 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import vector.Vector;
+
 class BoidJunitTest {
-
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-	}
-
-	@BeforeEach
-	void setUp() throws Exception {
-	}
-
-	@AfterEach
-	void tearDown() throws Exception {
-	}
 
 	@Test
 	void test() {
-		
+		Bird a = new Bird(10,10,new Vector(1,1));
 	}
 
 }
