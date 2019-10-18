a529d48fc515e5e87a2f867f7ea588e05513f2af
AlexanderLuasan <38295434+AlexanderLuasan@users.noreply.github.com>
2019-10-18 11:30:46 +0800

    document

--- flocking/src/graphics/Drawable.java
+++ flocking/src/graphics/Drawable.java
@@ -1,21 +1,16 @@
 package graphics;
 /*
  * unimplemented
- * class Drawable{
- * 
- * 
- * 		this one is not function discibable give a series of functions that allow you to draw differnt objects by requesting information
- * 		examples:
- *			note object may be made of multible shapes
- * 			String getColor(); //returns the color hash
- * 			bool isCircle(); // check to make sure they are a circle
- * 			int getCenterX();//returns a point
- * 			int getCenterY();
- * 			double getRadius();
- * 			with these functions you can detect and draw a circle of any color
- * 
- * 
- * }
+	class Drawable{
+
+ok		vector getCenter
+ok		Colors getColor
+ok		list drawable getdrawables
+ok		double getHeight
+ok		double getWidth
+ok 		double getRadius
+  
+ }
  * 
  * */
 
